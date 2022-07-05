e5a9bfe4928103ceeaf4d4da98757c5016d45780
vishal <vishaldeepverma@live.in>
2022-07-05 13:49:07 +0530

    database changes

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// (async function () {
-//   try {
-//     await sequelize.sync({ force: false });
-//     console.log("database Synced");
-//   } catch (error) {
-//     console.log(`got on error on sync db ${error}`);
-//   }
-// })();
+(async function () {
+  try {
+    await sequelize.sync({ force: false });
+    console.log("database Synced");
+  } catch (error) {
+    console.log(`got on error on sync db ${error}`);
+  }
+})();
 
 app.use(cors());
 
