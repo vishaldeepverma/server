0a93dc4f89064c734da7377ca36b44a6bafdf932
vishal <vishaldeepverma@live.in>
2022-07-05 18:21:49 +0530

    cors corrected

--- app.js
+++ app.js
@@ -21,12 +21,7 @@ app.use(express.urlencoded({ extended: false }));
   }
 })();
 
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api", router);
 
