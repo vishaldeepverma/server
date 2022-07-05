5207789a4c38b3fd4442076d598d771805a00558
vishal <vishaldeepverma@live.in>
2022-07-05 22:47:46 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const authentication = require('./auth');
 const user = require('./user');
 const usertheme = require('./usertheme');
 
-router.use('/v1/auth', authentication);
+router.use('/v1/auth', (req,res) => {
+    res.send("kopll");
+});
 router.use('/v1/user', user);
 router.use('/v1/usertheme', usertheme);
 
