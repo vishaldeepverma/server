8c09e395d8c2caf0d9027b36b4ed54c1f2fa9f40
vishal <vishaldeepverma@live.in>
2022-07-05 19:05:18 +0530

    change

--- app.js
+++ app.js
@@ -3,33 +3,18 @@ const dotenv = require("dotenv");
 
 const router = require("./routes/index");
 const { sequelize } = require("./models");
-// const cors = require("cors");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 
-// app.use(cors());
-// Add headers
-app.use(function (req, res, next) {
+var corsOptions = {
+  origin: 'https://silver-dango-9c6b90.netlify.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://silver-dango-9c6b90.netlify.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
