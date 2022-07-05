707cd0770c690a52842d64b7b3a9eb3ec0e1c6b4
vishal <vishaldeepverma@live.in>
2022-07-05 22:49:52 +0530

    test

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const dotenv = require("dotenv");
 
 const router = require("./routes/index");
 const { sequelize } = require("./models");
-// const cors = require("cors");
+const cors = require("cors");
 dotenv.config();
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,18 +26,6 @@ app.use("/api", router);
 
 app.get("/", (req, res, next) => {
   res.status(200).send("I am done");
-});
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  ); // If needed
-  res.setHeader("Access-Control-Allow-Headers", "*"); // If needed
-  res.setHeader("Access-Control-Allow-Credentials", true); // If needed
-  // Pass to next layer of middleware
-  next();
 });
 
 module.exports = app;
