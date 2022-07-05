2289139d07fc1912aa50c24de550d5d25082055a
vishal <vishaldeepverma@live.in>
2022-07-05 22:53:10 +0530

    fix

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors())
+app.options('*', cors()) // include before other routes
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
